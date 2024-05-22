3cea669a2fab115c0c89c9dfb6fe0f054066991f
Rana Asad Ali <ranaasadaly@gmail.com>
2024-05-22 14:42:22 +0500

    first commit

---
+++ demo/chat/src/constants.ts
@@ -0,0 +1,5 @@
+export const SETTINGS: Map<string, string> = new Map([
+  ["CONVAI-API-KEY", "b6463634282f3f063b80ae4d8607fa84"],
+  // ["CHARACTER-ID", "26f6542a-330f-11ee-b71c-42010a40000b"],
+  ["CHARACTER-ID", "a1e00af4-1811-11ef-b145-42010a7be00e"],
+]);
---
+++ demo/talking-avatar/src/constants.js
@@ -0,0 +1,5 @@
+export const SETTINGS = {
+    "CONVAI-API-KEY": "b6463634282f3f063b80ae4d8607fa84",
+    // "CHARACTER-ID": "37b0ede8-9ee0-11ed-97ae-42010a80000d"
+    "CHARACTER-ID": "a1e00af4-1811-11ef-b145-42010a7be00e"
+};
